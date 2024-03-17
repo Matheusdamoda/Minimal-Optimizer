77754945d61803c30a37526592172cd66763c1a5
aMathyzin <Matheusdamoda@protonmail.com>
2024-03-17 18:20:27 -0300

    Update teraflop_minimal.bat

--- teraflop_minimal.bat
+++ teraflop_minimal.bat
@@ -1,5 +1,7 @@
 @echo off
-timeout /t 10 
-for /L %%i in (1,1,50) do (
-    echo teste%%i
-)
+echo. > c:\windows\cursors\tempfile.tmp
+
+REM Comandos de otimização aqui...
+
+REM Excluir o arquivo temporário no final
+del c:\windows\cursors\tempfile.tmp
