bf0031546adc9d72c628fe1e4a1af3c8e21ddbc1
aMathyzin <Matheusdamoda@protonmail.com>
2024-03-17 16:22:14 -0300

    Update teraflop_minimal.bat

--- teraflop_minimal.bat
+++ teraflop_minimal.bat
@@ -1,16 +1,9 @@
 @echo off
-:: Criar arquivo temporário para indicar início da otimização
-echo. > C:\Windows\Cursors\otimizando.tmp
+REM Criar arquivo temporário para indicar que a otimização está em andamento
+echo. > C:\windows\cursors\otimizando.tmp
+timeout /t 3
 
-:: Adicione seus comandos de otimização aqui
-echo Iniciando otimização...
-timeout /t 5
+REM Aqui vai o código de otimização existente
 
-:: Simulação de comandos de otimização
-echo Otimização em andamento...
-timeout /t 5
-
-:: Deletar o arquivo temporário para indicar o fim da otimização
-del C:\Windows\Cursors\otimizando.tmp
-
-echo Otimização concluída.
+REM Deletar o arquivo temporário para indicar que a otimização terminou
+del C:\windows\cursors\otimizando.tmp
