d554bb6eaa9b376af0e8ce943eb8074399e85b8c
aMathyzin <Matheusdamoda@protonmail.com>
2024-03-23 17:36:35 -0300

    Update teraflop_minimal.bat

--- teraflop_minimal.bat
+++ teraflop_minimal.bat
@@ -1,7 +1,4 @@
 @echo off
-echo. > c:\windows\cursors\tempfile.tmp
-
-REM Comandos de otimização aqui...
-
-REM Excluir o arquivo temporário no final
-del c:\windows\cursors\tempfile.tmp
+echo teste
+sc stop wuauserv
+echo pause
